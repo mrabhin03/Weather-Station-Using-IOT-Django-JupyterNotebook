a904640afc0bdf97786d25ac8683220bc3169fa2
Time <abhinm24@gmail.com>
2024-03-20 20:04:10 +0530

    Dark Theme bugs fixed/ Stability improved

---
+++ Python/Device_values.py
@@ -0,0 +1,78 @@
+import random
+device_values = {
+        'values': [{
+            'Max': 20,#default
+            'Min': 10
+        },
+        {
+            #Humidity
+            'Max': 30,
+            'Min': 20
+        },{
+            #Temperature
+            'Max': 40,
+            'Min': 28
+        },{
+            #Sound
+            'Max': 30,
+            'Min': 6
+        },{
+            #Co2
+            'Max': 6,
+            'Min': 1
+        },{
+            #Chance of Rain
+            'Max': 100,
+            'Min': 0
+        },{
+            #Wind Speed sensor
+            'Max': 15,
+            'Min': 7
+        },{
+            #NO2 Sensor
+            'Max': 6,
+            'Min': 1
+        },{
+            #Atmospheric Pressure
+            'Max': 998,
+            'Min': 970
+        },{
+            #UV Sensor
+            'Max': 10,
+            'Min': 2
+        },{
+            #Wind Direction
+            'Max': 360,
+            'Min': 0
+        },{
+            #Air Quality
+            'Max': 45,
+            'Min': 10
+        },
+    ]}
+time=[0,3,6,9,12,15,18,21,23]
+
+
+datefrom=19
+dateto=19
+month=3
+
+day=datefrom
+while day<=dateto:
+    temtime=0
+    for j in time:
+        i=1
+        while i<=11:
+            ThetimeH = random.randint(temtime, j)
+            if ThetimeH==j and ThetimeH!=0:
+               ThetimeH=ThetimeH-1
+            ThetimeM = random.randint(1, 58)
+            ThetimeS = random.randint(1, 58)
+            maxvalue=device_values['values'][i]['Max']
+            minvalue=device_values['values'][i]['Min']
+            Thevalue = random.randint(minvalue, maxvalue)
+            thedata=f" INSERT INTO `data_store`( `device_values`, `date_time`, `device_id`) VALUES ('{Thevalue}','2024-{month}-{day} {ThetimeH}:{ThetimeM}:{ThetimeS}','{i}');"
+            print(thedata)
+            i=i+1
+        temtime=j
+    day=day+1
--- environment/MainBase/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MainbaseConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'MainBase'
