055fdbeaa8580ce99d7b4be2afff2b9dfadff497
Time <abhinm24@gmail.com>
2024-04-07 17:14:33 +0530

    Style Improved

--- Python/Device_values.py
+++ Python/Device_values.py
@@ -54,7 +54,7 @@ time=[0,3,6,9,12,15,18,21,23]
 
 
 datefrom=1
-dateto=4
+dateto=30
 month=4
 
 day=datefrom
@@ -62,12 +62,12 @@ while day<=dateto:
     temtime=0
     for j in time:
         i=1
+        ThetimeH = random.randint(temtime, j)
+        if ThetimeH==j and ThetimeH!=0:
+            ThetimeH=ThetimeH-1
+        ThetimeM = random.randint(10, 58)
+        ThetimeS = random.randint(10, 58)
         while i<=11:
-            ThetimeH = random.randint(temtime, j)
-            if ThetimeH==j and ThetimeH!=0:
-               ThetimeH=ThetimeH-1
-            ThetimeM = random.randint(1, 58)
-            ThetimeS = random.randint(1, 58)
             maxvalue=device_values['values'][i]['Max']
             minvalue=device_values['values'][i]['Min']
             Thevalue = random.randint(minvalue, maxvalue)
